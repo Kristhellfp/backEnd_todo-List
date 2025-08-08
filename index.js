be40c5127eb17c55262f1b2da58e6e3c575b2766
kristel <klfpaz@scl.edu.gt>
2025-08-08 10:30:05 -0600

    BackEnd inicios

---
+++ index.js
@@ -0,0 +1,17 @@
+
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("Mi backEnd con ExpressJS");
+});
+
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT,()=>{
+    console.log(`Servidor: http://localhost:${PORT}`);
+})
